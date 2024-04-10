e8452483774d8cba5ab9dc228f1dd2345bb292a5
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-10 20:22:06 +0530

    first commit

---
+++ src/app/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Body from '../components/Body'
+
+const Homepage = () => {
+  return (
+    <Fragment>
+      <Body/>
+    </Fragment>
+  )
+}
+
+export default Homepage
