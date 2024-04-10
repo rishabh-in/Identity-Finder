6f4fe7b6d77f57dfc5c5dc08538f1e529bff892b
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-10 21:27:27 +0530

    header and footer styling

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-10 flex justify-center items-center bg-cyan-100'>
+      <h1 className='font-extralight text-xl font-mono'>Footer</h1>
+    </div>
   )
 }
 
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex m-0 p-0 w-full h-20 bg-cyan-100 shadow-lg items-center'>
+    <div className='flex p-4 w-full h-20 bg-cyan-100 shadow-lg items-center'>
       <h1 className='font-extralight text-3xl ml-3 font-mono'>IdentityFinder Pro</h1>
     </div>
   )
