827460af88cfdd342fdf4a48775072e045582425
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-11 01:11:01 +0530

    Layout HOC - wrapper for pages

---
+++ src/utils/LayoutHOC.js
@@ -0,0 +1,15 @@
+import Footer from '@/app/components/Footer'
+import Header from '@/app/components/Header'
+import React from 'react'
+
+const LayoutHOC = ({children}) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  )
+}
+
+export default LayoutHOC
