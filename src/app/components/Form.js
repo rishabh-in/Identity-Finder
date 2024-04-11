47202d1818d77ecf6f64603a6fd1e3654290d300
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-11 12:10:49 +0530

    Search on enter press functionality added

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -9,10 +9,16 @@ const Form = ({handleSubmit}) => {
   const router = useRouter()
   const handleInputChange = e => setName(e.target.value);
 
+  const handleKeyPress = (e) => {
+    if(e.charCode === 13) {
+      handleSubmit(name)
+    }
+  }
+
   return (
     <div className='flex flex-col p-5 h-48 w-96 rounded-xl bg-white shadow-2xl justify-center items-center'>
-      <input onChange={(e) => handleInputChange(e)} value={name} className='outline-none h-12 text-xl border-2 rounded-lg font-mono' type='text' placeholder='Enter Name'/>
-      <button onClick={() => handleSubmit(name)} className='h-11 w-56 text-xl bg-blue-500 text-white font-mono mt-6 rounded-md hover:bg-opacity-80 transition-all ease-in-out duration-100'>Submit</button>
+      <input onChange={(e) => handleInputChange(e)} onKeyPress={(e) => handleKeyPress(e)} value={name} className='outline-none h-12 text-xl border-2 rounded-lg font-mono' type='text' placeholder='Enter Name'/>
+      <button onClick={() => handleSubmit(name)}  className='h-11 w-56 text-xl bg-blue-500 text-white font-mono mt-6 rounded-md hover:bg-opacity-80 transition-all ease-in-out duration-100'>Submit</button>
     </div>
   )
 }
